fe0f83bb55fb8bd707b4104f2eb3d307beed75eb
ioan ionac <ionaci@ioans-MacBook-Air.local>
2024-08-30 15:50:21 +0200

    ioan's third commit

--- src/components/DisplayE.jsx
+++ src/components/DisplayE.jsx
@@ -7,10 +7,23 @@ const DisplayE = ({ entry, setOpenModul }) => {
       <button className="btn">open modal</button>
       <dialog id="my_modal_1" className="modal modal-open">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img src={entry.image} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{entry.title}</h2>
+              <p className="py-4">
+                {new Date(entry.date).toLocaleString("de-DE", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}{" "}
+              </p>
+              <p>{entry.content}</p>
+            </div>
+          </div>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
