1be09247a5f149f1175a37547c0efb7a5cb97150
Albert Lazerson <albert@lazerson-design.de>
2024-08-30 11:41:49 +0200

    SuchfunktionBeiImagePicker

--- src/components/Imagepicker.jsx
+++ src/components/Imagepicker.jsx
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function ImagePicker({ onImageSelect }) {
   const [selectedImage, setSelectedImage] = useState(null);
-
-  const images = [
-    "https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp",
-    "https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp",
-    "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
-    "https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp",
-    "https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp",
-    "https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp",
-    "https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp",
-  ];
+  const [searchQuery, setSearchQuery] = useState("");
+  const [fetchedImages, setFetchedImages] = useState([]);
 
   const selectImage = (url) => {
     setSelectedImage(url);
@@ -21,6 +13,24 @@ function ImagePicker({ onImageSelect }) {
     if (selectedImage) {
       onImageSelect(selectedImage);
       document.getElementById("imagepicker_modal").close();
+    }
+  };
+
+  // Fetch images from Unsplash and log the result
+  const searchImages = async () => {
+    const accessKey = "K22tmtsdAY5-LyB9t5ZcrLMfXMKt40AAdXZEHNkqai0"; // access key
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${accessKey}&per_page=21`
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      const imageUrls = data.results.map((image) => image.urls.small); // Extract image URLs
+      setFetchedImages(imageUrls); // Update state with image URLs
+    } catch (error) {
+      console.error("Failed to fetch from Unsplash:", error);
     }
   };
 
@@ -42,65 +52,64 @@ function ImagePicker({ onImageSelect }) {
           </form>
           <h3 className="font-bold text-lg mb-4">Search Images</h3>
 
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full mb-4"
-          />
+          <div className="flex items-center justify-between mb-4">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered flex-grow"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button
+              onClick={searchImages}
+              className="btn btn-success text-white ml-4"
+            >
+              Search
+            </button>
+          </div>
 
           {/* Image Picker Content */}
           <div className="carousel w-full rounded-xl">
-            <div id="slide1" className="carousel-item relative w-full flex">
-              {images.slice(0, 3).map((url, index) => (
-                <div key={index} className="relative w-1/3">
-                  <img
-                    src={url}
-                    alt={`Image ${index + 1}`}
-                    className="w-full object-cover cursor-pointer"
-                    onClick={() => selectImage(url)}
-                  />
-                  {selectedImage === url && (
-                    <div className="absolute inset-0 bg-success opacity-60 flex items-center justify-center">
-                      <span className="text-white font-bold">Selected</span>
+            {Array.from({ length: 7 }).map((_, slideIndex) => (
+              <div
+                id={`slide${slideIndex + 1}`}
+                key={slideIndex}
+                className="carousel-item relative w-full flex"
+              >
+                {fetchedImages
+                  .slice(slideIndex * 3, slideIndex * 3 + 3)
+                  .map((url, index) => (
+                    <div key={index} className="relative w-1/3">
+                      <img
+                        src={url}
+                        alt={`Image ${slideIndex * 3 + index + 1}`}
+                        className="w-full object-cover cursor-pointer"
+                        onClick={() => selectImage(url)}
+                      />
+                      {selectedImage === url && (
+                        <div className="absolute inset-0 bg-success opacity-60 flex items-center justify-center">
+                          <span className="text-white font-bold">Selected</span>
+                        </div>
+                      )}
                     </div>
-                  )}
+                  ))}
+                {/* Navigation buttons */}
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                  <a
+                    href={`#slide${slideIndex === 0 ? 7 : slideIndex}`}
+                    className="btn btn-circle"
+                  >
+                    ❮
+                  </a>
+                  <a
+                    href={`#slide${slideIndex === 6 ? 1 : slideIndex + 2}`}
+                    className="btn btn-circle"
+                  >
+                    ❯
+                  </a>
                 </div>
-              ))}
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide2" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
               </div>
-            </div>
-
-            <div id="slide2" className="carousel-item relative w-full flex">
-              {images.slice(3, 6).map((url, index) => (
-                <div key={index} className="relative w-1/3">
-                  <img
-                    src={url}
-                    alt={`Image ${index + 4}`}
-                    className="w-full object-cover cursor-pointer"
-                    onClick={() => selectImage(url)}
-                  />
-                  {selectedImage === url && (
-                    <div className="absolute inset-0 bg-success opacity-60 flex items-center justify-center">
-                      <span className="text-white font-bold">Selected</span>
-                    </div>
-                  )}
-                </div>
-              ))}
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide1" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
 
           <div className="modal-action">
